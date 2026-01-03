b6a41c81203101c909983aab50b85bbdad55e3ab
Adam-SchoolAccount <adamvoss@student.purdueglobal.edu>
2026-01-03 14:28:53 -0600

    Add ATS.Core project with models, services, and XML data

---
+++ ATS_Application_Main/ATS.Core/ATS.Core/Models/Submission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ATS.Core.Models
+{
+    public class Submission
+    {
+        public int SubmissionID { get; set; }   // primary key (future sql)
+        public int StudentID { get; set; }  // FK to Student
+        public DateTime SubmissionTime { get; set; }
+
+        // set by server-side validation logic (API)
+        public bool Success { get; set; } 
+    }
+}
---
+++ ATS_Application_Main/ATS.Core/ATS.Core/Services/RosterService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using ATS.Core.Models;
+
+namespace ATS.Core.Services
+{
+
+    // service responsible for loading the roster from XML
+    // acts as a temporary data source until SQL is implemented
+    public class RosterService
+    {
+        public List<Student> LoadRoster(string xmlFilePath)
+        {
+            var serializer = new XmlSerializer(
+                typeof(List<Student>),
+                new XmlRootAttribute("Roster")
+             );
+
+            using var stream = new FileStream(xmlFilePath, FileMode.Open);
+            return (List<Student>)serializer.Deserialize(stream)!;
+
+
+        }
+    }
+}
+
+
+
+
+
+
