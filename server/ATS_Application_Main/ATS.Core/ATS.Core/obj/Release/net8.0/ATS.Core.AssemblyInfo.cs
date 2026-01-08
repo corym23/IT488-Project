11062e150aa5f716521fab50dc1e57329e763cc8
rocketpengu <jacquelinevo@protonmail.com>
2026-01-08 16:59:14 -0700

    Save work before pulling from main

--- server/ATS_Application_Main/ATS.Core/ATS.Core/obj/Release/net8.0/ATS.Core.AssemblyInfo.cs
+++ server/ATS_Application_Main/ATS.Core/ATS.Core/obj/Release/net8.0/ATS.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATS.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6cd5063f6117e01bb9bd07e5b93eb819618142")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8a80a4f0e4d8ed5fabcd630c4f07c80b63939a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATS.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATS.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
